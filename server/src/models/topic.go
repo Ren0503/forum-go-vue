c2a16264c30244cb6717f0ff5eff50320bbeeba9
Ren0503 <illuminati0440@gmail.com>
2022-01-17 19:32:28 +0700

    Add

---
+++ server/src/models/topic.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Topic struct {
+	ID            uint      `gorm:"primaryKey" json:"id"`
+	AuthorID      uint      `json:"-"`
+	Author        User      `json:"author"`
+	Title         string    `json:"title"`
+	Body          string    `json:"body"`
+	CreatedAt     time.Time `json:"createdAt"`
+	Likes         uint      `gorm:"-" json:"likes"`
+	Liked         bool      `gorm:"-" json:"liked"`
+	TopicActions  []Action  `gorm:"many2many:topic_actions;" json:"-"`
+	Comments      []Comment `gorm:"foreignKey:TopicID" json:"comments,omitempty"`
+	CommentsCount uint      `gorm:"-" json:"commentsCount"`
+}
---
+++ server/src/models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint      `gorm:"primaryKey,autoIncrement" json:"id"`
+	Username  string    `gorm:"index" json:"username"`
+	GivenName string    `json:"givenName"`
+	LastName  string    `json:"lastName"`
+	Admin     bool      `gorm:"index" json:"admin"`
+	CreatedAt time.Time `json:"createdAt"`
+	LastLogin time.Time `json:"-"`
+	Deleted   bool      `gorm:"index" json:"-"`
+
+	UserDetails UserDetails `gorm:"foreignKey:ID" json:"details,omitempty"`
+}
